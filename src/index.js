e3da798cd648da64d583a546b52f87b26bfcc2fc
David <davidmedina0907@gmail.com>
2023-08-19 00:21:33 -0700

    lab is complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './components/Form'
+import ParentCompoent from './components/ParentComponent';
 
 ReactDOM.render(
-  <Form />,
+  <ParentCompoent />,
   document.getElementById('root')
 );
